1ea9b621548cdff32a719109c6be1338bb7f9525
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2022-06-03 03:28:46 -0700

    On click zoom in(Non reversible)

--- mandelbrot-set/src/Canvas.js
+++ mandelbrot-set/src/Canvas.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react';
-class Canvas   extends Component {
+class Canvas extends Component {
     constructor(props) {
         super(props);
-        this.limit = 30;
-        this.scale = 0.02;
+        this.limit = 4;
+        this.scale = 0.005;
+        this.original = { x: 0, y: 0 };
         this.paint = this.paint.bind(this);
+        this.zoomIn = this.zoomIn.bind(this);
+        this.init = this.init.bind(this);
     }
 
-    componentDidMount(){
-        window.addEventListener("load",this.paint);
+    zoomIn(e) {
+        console.log(this.original);
+        var rectX = e.clientX - this.rectangle.left;
+        var rectY = e.clientY - this.rectangle.top;
+        console.log(rectX, rectY);
+        var x = -this.canvas.width / 2 + rectX;
+        var y = this.canvas.height / 2 - rectY;
+        console.log(x, y);
+        this.original.x += this.scale * (x);
+        this.original.y += this.scale * (y);
+        this.scale *= 0.5;
+        console.log(this.original);
+        this.paint();
     }
+
+    componentDidMount() {
+        window.addEventListener("load", () => { this.init(); this.paint() });
+    }
+
     state = {}
 
-    paint() {
+    init() {
         this.canvas = document.getElementById("canvas");
         this.context = this.canvas.getContext("2d");
         this.imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
-        var scale =0.001;
-        var position =0;
-        for (var x = -this.canvas.width/2; x < this.canvas.width/2; x++){
-            for (var y = -this.canvas.width/2; y < this.canvas.width/2 ; y++) {
-                // var ax = cx + x * scale;
-                // var ay = cy + y * scale;
-                var ax = x * scale;
-                var ay = y * scale;
+        this.rectangle = this.canvas.getBoundingClientRect();
+    }
+
+    paint() {
+        var position = 0;
+        for (var x = -this.canvas.width / 2; x < this.canvas.width / 2; x++) {
+            for (var y = -this.canvas.height / 2; y < this.canvas.height / 2; y++) {
+                var ax = this.original.x + x * this.scale;
+                var ay = this.original.y + y * this.scale;
 
                 var a1 = ax;
                 var b1 = ay;
@@ -39,7 +59,7 @@ class Canvas   extends Component {
                         break;
                 }
                 if (lp > 255) {
-                    lp = 0;
+                    lp = 255;
                 }
                 this.imageData.data[position] = lp;
                 this.imageData.data[position + 1] = lp;
@@ -50,10 +70,11 @@ class Canvas   extends Component {
         }
         this.context.putImageData(this.imageData, 0, 0);
     }
+
     render() {
-        var dimX = 840;
-        var dimY = 840;
-        return ( <canvas id="canvas" width={dimX} height={dimY}>  </canvas> );
+        var dimX = 640;
+        var dimY = 640;
+        return (<canvas style={{ border: "1px solid black" }} id="canvas" width={dimX} height={dimY} onClick={this.zoomIn}>  </canvas>);
 
     }
 }
